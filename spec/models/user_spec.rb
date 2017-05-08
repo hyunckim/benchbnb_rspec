3356277385c5ec6dea8fd8747b6e8ee6590147ef
hyunckim <hyunkim1209@gmail.com>
2017-05-07 22:46:05 -0700

    add factory girl to user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let! (:user) { User.create!(username: 'breakfast', password: 'password') }
+  let!(:user) { create(:user) }
 
   describe 'validations' do
 
